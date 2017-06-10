de1af32326ddd0748756ed4adb011b7689387468
Niceno Bojan <bojan.niceno@psi.ch>
2017-06-10 16:42:00 +0200

    Passed through re-indent.

--- constants/all.py
+++ constants/all.py
@@ -1,6 +1,6 @@
 """
-This is ruthless violation of Python's ethical code of conduct (best practice).  
-It includes all the constants defined in the code.  Very bad, but it makes 
+This is ruthless violation of Python's ethical code of conduct (best practice).
+It includes all the constants defined in the code.  Very bad, but it makes
 code development faster for non-IT oriendted minds.
 """
 
--- constants/solver.py
+++ constants/solver.py
@@ -3,7 +3,7 @@ This constant sets iterative linear solver (such as "bicgstab") tolerance.
 
 If very tight values are needed (often the case for pressure solution), one
 can use their product (TOL*TOL).  To loosen the tolerance up, sqrt(TOL) is
-possible.  
+possible.
 """
 
 # Solver tolerance
