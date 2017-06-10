91b8cfb3447287bd607db3677acd20528992ff46
Niceno Bojan <bojan.niceno@psi.ch>
2017-06-10 16:38:25 +0200

    Indentation set to four!

--- tests/test_obstacles_staggered.py
+++ tests/test_obstacles_staggered.py
@@ -1,12 +1,12 @@
 """
 #                                                       o ... scalars
-#                          (n)                          - ... u velocities      
+#                          (n)                          - ... u velocities
 #                                                       | ... v velocities
-#       +-------+-------+-------+-------+-------+          
-#       |       |       |       |       |       |            
-#       |   o   -   o   -   o   -   o   -   o   | j=ny-2     
-#       |       |       |       |       |       | 
-#       +---|---+---|---+---|---+---|---+---|---+     j=ny-1     
+#       +-------+-------+-------+-------+-------+
+#       |       |       |       |       |       |
+#       |   o   -   o   -   o   -   o   -   o   | j=ny-2
+#       |       |       |       |       |       |
+#       +---|---+---|---+---|---+---|---+---|---+     j=ny-1
 #       |       |       |       |       |       |
 #       |   o   -   o   -   o   -   o   -   o   | ...
 #       |       |       |       |       |       |
@@ -23,7 +23,7 @@
 # ^            i=0      i=1    ...    i=nx-2      (u-velocity cells)
 # |
 # +---> x                  (s)
-#  
+#
 """
 
 #!/usr/bin/python
@@ -39,58 +39,58 @@ from pyns.discretization.all  import *
 from pyns.physical_models.all import *
 
 def main(show_plot=True, time_steps=4800):
-  
+
 # =============================================================================
 #
 # Define problem
 #
 # =============================================================================
 
-  # Node coordinates
-  xn = nodes(0, 1,     256)
-  yn = nodes(0, 0.125,  32)
-  zn = nodes(0, 0.125,   4)
-  
-  # Cell coordinates
-  xc = avg(xn)
-  yc = avg(yn)
-  zc = avg(zn)
-  
-  # Cell dimensions
-  nx,ny,nz, dx,dy,dz, rc,ru,rv,rw = cartesian_grid(xn,yn,zn)
-  
-  # Set physical properties
-  rho, mu, cap, kappa = properties_for_air(rc)
-       
-  # Time-stepping parameters
-  dt  = 0.002      # time step
-  ndt = time_steps # number of time steps
-  
-  # Create unknowns; names, positions and sizes
-  uf = create_unknown('face-u-vel',  X, ru, DIRICHLET)
-  vf = create_unknown('face-v-vel',  Y, rv, DIRICHLET)
-  wf = create_unknown('face-w-vel',  Z, rw, DIRICHLET)
-  p  = create_unknown('pressure',    C, rc, NEUMANN)
-  
-  # Specify boundary conditions
-  uf.bnd[W].typ[:1,:,:] = DIRICHLET 
-  for k in range(0,nz):
-    uf.bnd[W].val[:1,:,k]  = par(0.1, yn)
-  
-  uf.bnd[E].typ[:1,:,:] = OUTLET 
-  
-  for j in (B,T):
-    uf.bnd[j].typ[:] = NEUMANN     
-    vf.bnd[j].typ[:] = NEUMANN     
-    wf.bnd[j].typ[:] = NEUMANN     
-  
-  adj_n_bnds(p)
-  
-  obst = zeros(rc)
-  for j in range(0, 24):
-    for i in range(64+j, 64+24):
-        for k in range(0,nz):
-          obst[i,j,k] = 1
+    # Node coordinates
+    xn = nodes(0, 1,     256)
+    yn = nodes(0, 0.125,  32)
+    zn = nodes(0, 0.125,   4)
+
+    # Cell coordinates
+    xc = avg(xn)
+    yc = avg(yn)
+    zc = avg(zn)
+
+    # Cell dimensions
+    nx,ny,nz, dx,dy,dz, rc,ru,rv,rw = cartesian_grid(xn,yn,zn)
+
+    # Set physical properties
+    rho, mu, cap, kappa = properties_for_air(rc)
+
+    # Time-stepping parameters
+    dt  = 0.002      # time step
+    ndt = time_steps # number of time steps
+
+    # Create unknowns; names, positions and sizes
+    uf = create_unknown('face-u-vel',  X, ru, DIRICHLET)
+    vf = create_unknown('face-v-vel',  Y, rv, DIRICHLET)
+    wf = create_unknown('face-w-vel',  Z, rw, DIRICHLET)
+    p  = create_unknown('pressure',    C, rc, NEUMANN)
+
+    # Specify boundary conditions
+    uf.bnd[W].typ[:1,:,:] = DIRICHLET
+    for k in range(0,nz):
+        uf.bnd[W].val[:1,:,k]  = par(0.1, yn)
+
+    uf.bnd[E].typ[:1,:,:] = OUTLET
+
+    for j in (B,T):
+        uf.bnd[j].typ[:] = NEUMANN
+        vf.bnd[j].typ[:] = NEUMANN
+        wf.bnd[j].typ[:] = NEUMANN
+
+    adj_n_bnds(p)
+
+    obst = zeros(rc)
+    for j in range(0, 24):
+        for i in range(64+j, 64+24):
+            for k in range(0,nz):
+                obst[i,j,k] = 1
 
 # =============================================================================
 #
@@ -98,56 +98,56 @@ def main(show_plot=True, time_steps=4800):
 #
 # =============================================================================
 
-  # ----------
-  #
-  # Time loop 
-  #
-  # ----------
-  for ts in range(1,ndt+1):
-  
-    print_time_step(ts)
-    
-    # -----------------
-    # Store old values
-    # -----------------
-    uf.old[:] = uf.val[:]
-    vf.old[:] = vf.val[:]
-    wf.old[:] = wf.val[:]
-    
-    # ----------------------
-    # Momentum conservation
-    # ----------------------
-    ef = zeros(ru), zeros(rv), zeros(rw)
-      
-    calc_uvw((uf,vf,wf), (uf,vf,wf), rho, mu,  \
-             zeros(rc), ef, dt, (dx,dy,dz), obst)
-    
-    # ---------
-    # Pressure
-    # ---------
-    calc_p(p, (uf,vf,wf), rho, dt, (dx,dy,dz), obst)
-    
-    # --------------------
-    # Velocity correction
-    # --------------------
-    corr_uvw((uf,vf,wf), p, rho, dt, (dx,dy,dz), obst)
-   
-    # Compute volume balance for checking 
-    err = vol_balance((uf,vf,wf), (dx,dy,dz), obst)
-    print('Maximum volume error after correction: %12.5e' % abs(err).max())
-  
-    # Check the CFL number too 
-    cfl = cfl_max((uf,vf,wf), dt, (dx,dy,dz))
-    print('Maximum CFL number: %12.5e' % cfl)
+    # ----------
+    #
+    # Time loop
+    #
+    # ----------
+    for ts in range(1,ndt+1):
+
+        print_time_step(ts)
+
+        # -----------------
+        # Store old values
+        # -----------------
+        uf.old[:] = uf.val[:]
+        vf.old[:] = vf.val[:]
+        wf.old[:] = wf.val[:]
+
+        # ----------------------
+        # Momentum conservation
+        # ----------------------
+        ef = zeros(ru), zeros(rv), zeros(rw)
+
+        calc_uvw((uf,vf,wf), (uf,vf,wf), rho, mu,  \
+                 zeros(rc), ef, dt, (dx,dy,dz), obst)
+
+        # ---------
+        # Pressure
+        # ---------
+        calc_p(p, (uf,vf,wf), rho, dt, (dx,dy,dz), obst)
+
+        # --------------------
+        # Velocity correction
+        # --------------------
+        corr_uvw((uf,vf,wf), p, rho, dt, (dx,dy,dz), obst)
+
+        # Compute volume balance for checking
+        err = vol_balance((uf,vf,wf), (dx,dy,dz), obst)
+        print('Maximum volume error after correction: %12.5e' % abs(err).max())
+
+        # Check the CFL number too
+        cfl = cfl_max((uf,vf,wf), dt, (dx,dy,dz))
+        print('Maximum CFL number: %12.5e' % cfl)
 
 # =============================================================================
 #
 # Visualisation
 #
 # =============================================================================
-    if show_plot:
-      if ts % 20 == 0:
-        plot_isolines(p.val, (uf,vf,wf), (xn,yn,zn), Z)
+        if show_plot:
+            if ts % 20 == 0:
+                plot_isolines(p.val, (uf,vf,wf), (xn,yn,zn), Z)
 
 if __name__ == '__main__':
     main()
--- tests/test_thermal_plumes_collocated.py
+++ tests/test_thermal_plumes_collocated.py
@@ -22,97 +22,97 @@ from pyns.discretization.all  import *
 from pyns.physical_models.all import *
 
 def main(show_plot=True, time_steps=1800):
-  
+
 # =============================================================================
 #
 # Define problem
 #
 # =============================================================================
 
-  # Node coordinates
-  xn = nodes(0, 10, 300)
-  yn = nodes(0,  1,  40, 1/500, 1/500)
-  zn = nodes(0,  3,   3)
-  
-  # Cell coordinates
-  xc = avg(xn)
-  yc = avg(yn)
-  zc = avg(zn)
-  
-  # Cell dimensions
-  nx,ny,nz, dx,dy,dz, rc,ru,rv,rw = cartesian_grid(xn,yn,zn)
-  
-  # Set physical properties
-  rho   = zeros(rc)
-  mu    = zeros(rc)
-  kappa = zeros(rc)
-  cap   = zeros(rc)
-  rho  [:,:,:] = 1. 
-  mu   [:,:,:] = 0.1
-  kappa[:,:,:] = 0.15 
-  cap  [:,:,:] = 1.0 
-       
-  # Time-stepping parameters
-  dt  = 0.003       # time step
-  ndt = time_steps  # number of time steps
-  
-  # Create unknowns; names, positions and sizes
-  uc = create_unknown('cell-u-vel',  C, rc, DIRICHLET)
-  vc = create_unknown('cell-v-vel',  C, rc, DIRICHLET)
-  wc = create_unknown('cell-w-vel',  C, rc, DIRICHLET)
-  uf = create_unknown('face-u-vel',  X, ru, DIRICHLET)
-  vf = create_unknown('face-v-vel',  Y, rv, DIRICHLET)
-  wf = create_unknown('face-w-vel',  Z, rw, DIRICHLET)
-  t  = create_unknown('temperature', C, rc, NEUMANN)
-  p  = create_unknown('pressure',    C, rc, NEUMANN)
-  
-  # Specify boundary conditions
-  uc.bnd[W].typ[:1,:,:] = DIRICHLET 
-  for k in range(0,nz):
-    uc.bnd[W].val[:1,:,k] = par(1.0, yn);
-  
-  uc.bnd[E].typ[:1,:,:] = OUTLET 
-  uc.bnd[E].val[:1,:,:] = 1.0;
-  
-  for j in (B,T):
-    uc.bnd[j].typ[:] = NEUMANN     
-    vc.bnd[j].typ[:] = NEUMANN     
-    wc.bnd[j].typ[:] = NEUMANN     
-  
-  t.bnd[W].typ[:1,:,:] = DIRICHLET
-  for k in range(0,nz):
-    t.bnd[W].val[:1,:,k] = 1.0-yc;
-  
-  t.bnd[S].typ[:,:1,:] = DIRICHLET 
-  t.bnd[S].val[:,:1,:] = +1.0;
-  t.bnd[N].typ[:,:1,:] = DIRICHLET 
-  t.bnd[N].val[:,:1,:] =  0.0
-  
-  adj_n_bnds(t)
-  adj_n_bnds(p)
-  
-  # Specify initial conditions
-  uc.val[:,:,:] = 1.0
-  t.val[:,:,:] = 0
-  
-  # Copy the values to face velocities 
-  uf.val[:] = avg(X, uc.val)
-  vf.val[:] = avg(Y, vc.val)
-  wf.val[:] = avg(Z, wc.val)
-  for j in (W,E):
-    uf.bnd[j].val[:] = uc.bnd[j].val[:]  
-    vf.bnd[j].val[:] = avg(Y, vc.bnd[j].val[:])
-    wf.bnd[j].val[:] = avg(Z, wc.bnd[j].val[:])  
-  for j in (S,N):
-    uf.bnd[j].val[:] = avg(X, uc.bnd[j].val[:])
-    vf.bnd[j].val[:] = vc.bnd[j].val[:]
-    wf.bnd[j].val[:] = avg(Z, wc.bnd[j].val[:])  
-  for j in (B,T):  
-    uf.bnd[j].val[:] = avg(X, uc.bnd[j].val[:])
-    vf.bnd[j].val[:] = avg(Y, vc.bnd[j].val[:])
-    wf.bnd[j].val[:] = wc.bnd[j].val[:]  
-            
-  obst = zeros(rc)
+    # Node coordinates
+    xn = nodes(0, 10, 300)
+    yn = nodes(0,  1,  40, 1/500, 1/500)
+    zn = nodes(0,  3,   3)
+
+    # Cell coordinates
+    xc = avg(xn)
+    yc = avg(yn)
+    zc = avg(zn)
+
+    # Cell dimensions
+    nx,ny,nz, dx,dy,dz, rc,ru,rv,rw = cartesian_grid(xn,yn,zn)
+
+    # Set physical properties
+    rho   = zeros(rc)
+    mu    = zeros(rc)
+    kappa = zeros(rc)
+    cap   = zeros(rc)
+    rho  [:,:,:] = 1.
+    mu   [:,:,:] = 0.1
+    kappa[:,:,:] = 0.15
+    cap  [:,:,:] = 1.0
+
+    # Time-stepping parameters
+    dt  = 0.003       # time step
+    ndt = time_steps  # number of time steps
+
+    # Create unknowns; names, positions and sizes
+    uc = create_unknown('cell-u-vel',  C, rc, DIRICHLET)
+    vc = create_unknown('cell-v-vel',  C, rc, DIRICHLET)
+    wc = create_unknown('cell-w-vel',  C, rc, DIRICHLET)
+    uf = create_unknown('face-u-vel',  X, ru, DIRICHLET)
+    vf = create_unknown('face-v-vel',  Y, rv, DIRICHLET)
+    wf = create_unknown('face-w-vel',  Z, rw, DIRICHLET)
+    t  = create_unknown('temperature', C, rc, NEUMANN)
+    p  = create_unknown('pressure',    C, rc, NEUMANN)
+
+    # Specify boundary conditions
+    uc.bnd[W].typ[:1,:,:] = DIRICHLET
+    for k in range(0,nz):
+        uc.bnd[W].val[:1,:,k] = par(1.0, yn);
+
+    uc.bnd[E].typ[:1,:,:] = OUTLET
+    uc.bnd[E].val[:1,:,:] = 1.0;
+
+    for j in (B,T):
+        uc.bnd[j].typ[:] = NEUMANN
+        vc.bnd[j].typ[:] = NEUMANN
+        wc.bnd[j].typ[:] = NEUMANN
+
+    t.bnd[W].typ[:1,:,:] = DIRICHLET
+    for k in range(0,nz):
+        t.bnd[W].val[:1,:,k] = 1.0-yc;
+
+    t.bnd[S].typ[:,:1,:] = DIRICHLET
+    t.bnd[S].val[:,:1,:] = +1.0;
+    t.bnd[N].typ[:,:1,:] = DIRICHLET
+    t.bnd[N].val[:,:1,:] =  0.0
+
+    adj_n_bnds(t)
+    adj_n_bnds(p)
+
+    # Specify initial conditions
+    uc.val[:,:,:] = 1.0
+    t.val[:,:,:] = 0
+
+    # Copy the values to face velocities
+    uf.val[:] = avg(X, uc.val)
+    vf.val[:] = avg(Y, vc.val)
+    wf.val[:] = avg(Z, wc.val)
+    for j in (W,E):
+        uf.bnd[j].val[:] = uc.bnd[j].val[:]
+        vf.bnd[j].val[:] = avg(Y, vc.bnd[j].val[:])
+        wf.bnd[j].val[:] = avg(Z, wc.bnd[j].val[:])
+    for j in (S,N):
+        uf.bnd[j].val[:] = avg(X, uc.bnd[j].val[:])
+        vf.bnd[j].val[:] = vc.bnd[j].val[:]
+        wf.bnd[j].val[:] = avg(Z, wc.bnd[j].val[:])
+    for j in (B,T):
+        uf.bnd[j].val[:] = avg(X, uc.bnd[j].val[:])
+        vf.bnd[j].val[:] = avg(Y, vc.bnd[j].val[:])
+        wf.bnd[j].val[:] = wc.bnd[j].val[:]
+
+    obst = zeros(rc)
 
 # =============================================================================
 #
@@ -120,64 +120,64 @@ def main(show_plot=True, time_steps=1800):
 #
 # =============================================================================
 
-  # ----------
-  #
-  # Time loop 
-  #
-  # ----------
-  for ts in range(1,ndt+1):
-  
-    print_time_step(ts)
-    
-    # -----------------
-    # Store old values
-    # -----------------
-    t.old[:]  = t.val[:]
-    uc.old[:] = uc.val[:]
-    vc.old[:] = vc.val[:]
-    wc.old[:] = wc.val[:]
-    
-    # -----------------------
-    # Temperature (enthalpy)
-    # -----------------------
-    calc_t(t, (uf,vf,wf), (rho*cap), kappa, dt, (dx,dy,dz), obst)
-  
-    # ----------------------
-    # Momentum conservation
-    # ----------------------
-    ef = zeros(rc), 150.0 * t.val, zeros(rc)
-      
-    calc_uvw((uc,vc,wc), (uf,vf,wf), rho, mu,  \
-             zeros(rc), ef, dt, (dx,dy,dz), obst)
-    
-    # ---------
-    # Pressure
-    # ---------
-    calc_p(p, (uf,vf,wf), rho, dt, (dx,dy,dz), obst)
-    
-    # --------------------
-    # Velocity correction
-    # --------------------
-    corr_uvw((uc,vc,wc), p, rho, dt, (dx,dy,dz), obst)
-    corr_uvw((uf,vf,wf), p, rho, dt, (dx,dy,dz), obst)
-   
-    # Compute volume balance for checking 
-    err = vol_balance((uf,vf,wf), (dx,dy,dz), obst)
-    print('Maximum volume error after correction: %12.5e' % abs(err).max())
-  
-    # Check the CFL number too 
-    cfl = cfl_max((uc,vc,wc), dt, (dx,dy,dz))
-    print('Maximum CFL number: %12.5e' % cfl)
+    # ----------
+    #
+    # Time loop
+    #
+    # ----------
+    for ts in range(1,ndt+1):
+
+        print_time_step(ts)
+
+        # -----------------
+        # Store old values
+        # -----------------
+        t.old[:]  = t.val[:]
+        uc.old[:] = uc.val[:]
+        vc.old[:] = vc.val[:]
+        wc.old[:] = wc.val[:]
+
+        # -----------------------
+        # Temperature (enthalpy)
+        # -----------------------
+        calc_t(t, (uf,vf,wf), (rho*cap), kappa, dt, (dx,dy,dz), obst)
+
+        # ----------------------
+        # Momentum conservation
+        # ----------------------
+        ef = zeros(rc), 150.0 * t.val, zeros(rc)
+
+        calc_uvw((uc,vc,wc), (uf,vf,wf), rho, mu,  \
+                 zeros(rc), ef, dt, (dx,dy,dz), obst)
+
+        # ---------
+        # Pressure
+        # ---------
+        calc_p(p, (uf,vf,wf), rho, dt, (dx,dy,dz), obst)
+
+        # --------------------
+        # Velocity correction
+        # --------------------
+        corr_uvw((uc,vc,wc), p, rho, dt, (dx,dy,dz), obst)
+        corr_uvw((uf,vf,wf), p, rho, dt, (dx,dy,dz), obst)
+
+        # Compute volume balance for checking
+        err = vol_balance((uf,vf,wf), (dx,dy,dz), obst)
+        print('Maximum volume error after correction: %12.5e' % abs(err).max())
+
+        # Check the CFL number too
+        cfl = cfl_max((uc,vc,wc), dt, (dx,dy,dz))
+        print('Maximum CFL number: %12.5e' % cfl)
 
 # =============================================================================
 #
 # Visualisation
 #
 # =============================================================================
-    if show_plot:
-      if ts % 150 == 0:
-        plot_isolines(t.val, (uc,vc,wc), (xn,yn,zn), Z)
-        plot_isolines(p.val, (uc,vc,wc), (xn,yn,zn), Z)
+        if show_plot:
+            if ts % 150 == 0:
+                plot_isolines(t.val, (uc,vc,wc), (xn,yn,zn), Z)
+                plot_isolines(p.val, (uc,vc,wc), (xn,yn,zn), Z)
 
 if __name__ == '__main__':
     main()
