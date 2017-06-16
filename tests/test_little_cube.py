4ae3c700704d72525500ca1c85e56c7eb64f77ac
Bojan Niceno <bojan.niceno@psi.ch>
2017-06-16 14:53:23 +0200

    Added to repository. In the future, it will evolve into periodic case.

---
+++ tests/test_little_cube.py
@@ -0,0 +1,153 @@
+"""
+#                                                       o ... scalars
+#                          (n)                          - ... u velocities
+#                                                       | ... v velocities
+#       +-------+-------+-------+-------+-------+
+#       |       |       |       |       |       |
+#       |   o   -   o   -   o   -   o   -   o   | j=ny-1
+#       |       |       |       |       |       |
+#       +---|---+---|---+---|---+---|---+---|---+     j=ny-2
+#       |       |       |       |       |       |
+#       |   o   -   o   -   o   -   o   -   o   | ...
+#       |       |       |       |       |       |
+#  (w)  +---|---+---|---+---|---+---|---+---|---+    j=1        (e)
+#       |       |       |       |       |       |
+#       |   o   -   o   -   o   -   o   -   o   | j=1
+#       |       |       |       |       |       |
+#       +---|---+---|---+---|---+---|---+---|---+    j=0 (v-velocity)
+#       |       |       |       |       |       |
+#       |   o   -   o   -   o   -   o   -   o   | j=0   (scalar cell)
+#       |       |       |       |       |       |
+#       +-------+-------+-------+-------+-------+
+#  y       i=0     i=1     ...     ...    i=nx-1      (scalar cells)
+# ^            i=0      i=1    ...    i=nx-2      (u-velocity cells)
+# |
+# +---> x                  (s)
+#
+"""
+
+#!/usr/bin/python
+
+# Standard Python modules
+from pyns.standard import *
+
+# PyNS modules
+from pyns.constants      import *
+from pyns.operators      import *
+from pyns.discretization import *
+from pyns.display        import plot, write
+from pyns.physical       import properties
+
+def main(show_plot=True, time_steps=1000, plot_freq=10):
+
+# =============================================================================
+#
+# Define problem
+#
+# =============================================================================
+
+    # Node coordinates
+    xn = nodes(0, 0.8, 80)
+    yn = nodes(0, 0.6, 60)
+    zn = nodes(0, 0.3, 30)
+
+    # Cell coordinates
+    xc = avg(xn)
+    yc = avg(yn)
+    zc = avg(zn)
+
+    # Cell dimensions
+    nx,ny,nz, dx,dy,dz, rc,ru,rv,rw = cartesian_grid(xn,yn,zn)
+
+    # Set physical properties
+    rho, mu, cap, kappa = properties.air(rc)
+
+    # Time-stepping parameters
+    dt  = 0.02        # time step
+    ndt = time_steps  # number of time steps
+
+    # Create unknowns; names, positions and sizes
+    uf = Unknown('face-u-vel',  X, ru, DIRICHLET)
+    vf = Unknown('face-v-vel',  Y, rv, DIRICHLET)
+    wf = Unknown('face-w-vel',  Z, rw, DIRICHLET)
+    p  = Unknown('pressure',    C, rc, NEUMANN)
+
+    # Specify boundary conditions
+    uf.bnd[W].typ[:1,:,:] = DIRICHLET
+    for j in range(0, ny):
+        uf.bnd[W].val[:1,j,:]  = par(0.1, zn)
+
+    uf.bnd[E].typ[:1,:,:] = OUTLET
+
+    for j in (S, N):
+        uf.bnd[j].typ[:] = NEUMANN
+        vf.bnd[j].typ[:] = NEUMANN
+        wf.bnd[j].typ[:] = NEUMANN
+
+    obst = zeros(rc)
+    for j in range(22, 38):
+        for i in range(22, 38):
+            for k in range(0,16):
+                obst[i,j,k] = 1
+
+# =============================================================================
+#
+# Solution algorithm
+#
+# =============================================================================
+
+    # ----------
+    #
+    # Time loop
+    #
+    # ----------
+    for ts in range(1,ndt+1):
+
+        write.time_step(ts)
+
+        # -----------------
+        # Store old values
+        # -----------------
+        uf.old[:] = uf.val[:]
+        vf.old[:] = vf.val[:]
+        wf.old[:] = wf.val[:]
+
+        # ----------------------
+        # Momentum conservation
+        # ----------------------
+        ef = zeros(ru), zeros(rv), zeros(rw)
+
+        calc_uvw((uf,vf,wf), (uf,vf,wf), rho, mu,  \
+                 zeros(rc), ef, dt, (dx,dy,dz), obst)
+
+        # ---------
+        # Pressure
+        # ---------
+        calc_p(p, (uf,vf,wf), rho, dt, (dx,dy,dz), obst)
+
+        # --------------------
+        # Velocity correction
+        # --------------------
+        corr_uvw((uf,vf,wf), p, rho, dt, (dx,dy,dz), obst)
+
+        # Compute volume balance for checking
+        err = vol_balance((uf,vf,wf), (dx,dy,dz), obst)
+        print('Maximum volume error after correction: %12.5e' % abs(err).max())
+
+        # Check the CFL number too
+        cfl = cfl_max((uf,vf,wf), dt, (dx,dy,dz))
+        print('Maximum CFL number: %12.5e' % cfl)
+
+# =============================================================================
+#
+# Visualisation
+#
+# =============================================================================
+        if show_plot:
+            if ts % plot_freq == 0:
+                plot.isolines(p.val, (uf,vf,wf), (xn,yn,zn), Z)
+                plot.tecplot("cube-periodicity-%6.6d.plt" % ts, 
+                             (xn, yn, zn), (uf, vf, wf, p))
+
+if __name__ == '__main__':
+    main()
