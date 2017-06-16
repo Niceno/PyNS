a0c451014ddc4bbfd801a0cca1b28439a6749be1
Bojan Niceno <bojan.niceno@psi.ch>
2017-06-16 12:28:34 +0200

    Added "link" functions, for linking values in periodic domains.

--- operators/__init__.py
+++ operators/__init__.py
@@ -5,16 +5,18 @@ Very bad, but it makes code development faster for non-IT oriendted minds.
 """
 
 # Operators
-from .avg   import avg
-from .avg_x import avg_x
-from .avg_y import avg_y
-from .avg_z import avg_z
-from .cat   import cat
-from .cat_x import cat_x
-from .cat_y import cat_y
-from .cat_z import cat_z
-from .dif   import dif
-from .dif_x import dif_x
-from .dif_y import dif_y
-from .dif_z import dif_z
-from .par   import par
+from .avg      import avg
+from .avg_x    import avg_x
+from .avg_y    import avg_y
+from .avg_z    import avg_z
+from .cat      import cat
+from .cat_x    import cat_x
+from .cat_y    import cat_y
+from .cat_z    import cat_z
+from .dif      import dif
+from .dif_x    import dif_x
+from .dif_y    import dif_y
+from .dif_z    import dif_z
+from .par      import par
+from .link_add import link_add_x, link_add_y, link_add_z
+from .link_avg import link_avg_x, link_avg_y, link_avg_z
