a00e1c262f362db6f49ec6904f46e31b528bce56
Bojan Niceno <bojan.niceno@psi.ch>
2017-06-16 12:24:57 +0200

    Performs tests with periodicity in all directions.

--- demo/demo_advection_1D.py
+++ demo/demo_advection_1D.py
@@ -35,20 +35,24 @@ TEST = test[ floor( random() * len(test) ) ]
 MIN = 10
 MAX = 20
 
-uvw_bulk = SENS, 0.0, 0.0
-xn = nodes(0, 1,   200)
-yn = nodes(0, 0.1,   4)
-zn = nodes(0, 0.1,   4)
+if TEST == X:
+    uvw_bulk = SENS, 0.0, 0.0
+    xn  = nodes(0, 1,   200)
+    yn  = nodes(0, 0.1,   4)
+    zn  = nodes(0, 0.1,   4)
+    per = (True, False, False)
 if TEST == Y:
-  uvw_bulk = 0.0, SENS, 0.0
-  xn = nodes(0, 0.1,   4)
-  yn = nodes(0, 1,   200)
-  zn = nodes(0, 0.1,   4)
+    uvw_bulk = 0.0, SENS, 0.0
+    xn  = nodes(0, 0.1,   4)
+    yn  = nodes(0, 1,   200)
+    zn  = nodes(0, 0.1,   4)
+    per = (False, True, False)
 elif TEST == Z:
-  uvw_bulk = 0.0, 0.0, SENS
-  xn = nodes(0, 0.1,   4)
-  yn = nodes(0, 0.1,   4)
-  zn = nodes(0, 1,   200)
+    uvw_bulk = 0.0, 0.0, SENS
+    xn  = nodes(0, 0.1,   4)
+    yn  = nodes(0, 0.1,   4)
+    zn  = nodes(0, 1,   200)
+    per = (False, False, True)
 
 # Set domain dimensions and grid resolution
 
@@ -66,14 +70,14 @@ kappa[:,:,:] = 0.0
 cap  [:,:,:] = 1.0 
 
 # Time-stepping parameters
-dt     =   0.002;     # time step
-ndt    = 350;         # number of time steps
+dt     =   0.0025;     # time step
+ndt    = 180;         # number of time steps
 
 # Create unknowns; names, positions and sizes
-uf = create_unknown('face-u-vel',  X, ru, DIRICHLET)
-vf = create_unknown('face-v-vel',  Y, rv, DIRICHLET)
-wf = create_unknown('face-w-vel',  Z, rw, DIRICHLET)
-t  = create_unknown('temperature', C, rc, NEUMANN)
+uf = Unknown('face-u-vel',  X, ru, DIRICHLET)
+vf = Unknown('face-v-vel',  Y, rv, DIRICHLET)
+wf = Unknown('face-w-vel',  Z, rw, DIRICHLET)
+t  = Unknown('temperature', Y, rv, NEUMANN, per)
 
 # Make a tuple with all velocity components
 uvwf = uf, vf, wf
@@ -81,19 +85,19 @@ uvwf = uf, vf, wf
 # Specify inital and boundary conditions
 t.val[:] = MIN
 if TEST == X:
-  t.val[nx/2-25:nx/2+25,:,:] = MAX
+    t.val[nx//2-25:nx//2+25,:,:] = MAX
 elif TEST == Y:
-  t.val[:,ny/2-25:ny/2+25,:] = MAX
+    t.val[:,ny//2-25:ny//2+25,:] = MAX
 elif TEST == Z:
-  t.val[:,:,nz/2-25:nz/2+25] = MAX
+    t.val[:,:,nz//2-25:nz//2+25] = MAX
 
 adj_n_bnds(t)
 
 # This is from the advection-1D case
 for i in (X,Y,Z):
-  uvwf[i].val[:,:,:] = uvw_bulk[i]
-  for j in (W,E,S,N,B,T):
-    uvwf[i].bnd[j].val[:,:,:] = uvw_bulk[i]
+    uvwf[i].val[:,:,:] = uvw_bulk[i]
+    for j in (W,E,S,N,B,T):
+        uvwf[i].bnd[j].val[:,:,:] = uvw_bulk[i]
 
 obst = zeros(rc)
 
@@ -108,19 +112,19 @@ obst = zeros(rc)
 # Time loop 
 #
 # ----------
-for ts in range(1,40):
+for ts in range(1,ndt):
 
-  write.time_step(ts)
-  
-  # -----------------
-  # Store old values
-  # -----------------
-  t.old[:] = t.val[:]
-  
-  # -----------------------
-  # Temperature (enthalpy)
-  # -----------------------
-  calc_t(t, uvwf, (rho*cap), kappa, dt, (dx,dy,dz), obst)
+    write.time_step(ts)
+    
+    # -----------------
+    # Store old values
+    # -----------------
+    t.old[:] = t.val[:]
+    
+    # -----------------------
+    # Temperature (enthalpy)
+    # -----------------------
+    calc_t(t, uvwf, (rho*cap), kappa, dt, (dx,dy,dz), obst)
   
 # =============================================================================
 #
@@ -131,15 +135,18 @@ from matplotlib  import pyplot as plt
 
 st = 'ro'
 xc = avg(xn)
-vc = t.val[:,ny/2,nz/2]
+vc = t.val[:,ny//2,nz//2]
 if TEST == Y:
-  st = 'go'
-  xc = avg(yn)
-  vc = t.val[nx/2,:,nz/2]
+    st = 'go'
+    xc = avg(yn)
+    vc = t.val[nx//2,:,nz//2]
 elif TEST == Z:
-  st = 'bo'
-  xc = avg(zn)
-  vc = t.val[nx/2,ny/2,:]
+    st = 'bo'
+    xc = avg(zn)
+    vc = t.val[nx//2,ny//2,:]
 
+if t.pos == TEST:
+    xc = avg(xc)
+    
 plt.plot(xc, vc, st)
 plt.show()  
