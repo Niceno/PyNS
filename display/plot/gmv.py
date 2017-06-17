054b4d92bfb4c745e5ef7d3c8615707990614bd3
Niceno Bojan <bojan.niceno@psi.ch>
2017-06-17 16:18:02 +0200

    Now able to plot general three-dimensional fields, not just Unknowns.

--- display/plot/gmv.py
+++ display/plot/gmv.py
@@ -13,15 +13,15 @@ from pyns.constants import *
 from pyns.operators import *
 
 # =============================================================================
-def gmv(file_name, xyzn, variables):
+def gmv(file_name, xyzn, vars):
 # -----------------------------------------------------------------------------
     """
     Args:
       file_name: String containing name of the file to be created.
       xyzn:      Tuple containing one-dimensional arrays with "x", "y"
                  and "z" coordinates.
-      variables: Tuple containing unknowns to be exported to GMV (TM).
-                 Individual variables can be either collocated or staggered.
+      vars:      Tuple containing "Unknowns" to be exported to GMV (TM).
+                 Individual unknowns can be either collocated or staggered.
 
     Returns:
       none!
@@ -128,3 +128,113 @@ def gmv(file_name, xyzn, variables):
     file_id.close()
 
     return  # end of function
+    
+# =============================================================================
+def gmv(file_name, xyzn, var, pos):
+# -----------------------------------------------------------------------------
+    """
+    Args:
+      file_name: String containing name of the file to be created.
+      xyzn:      Tuple containing one-dimensional arrays with "x", "y"
+                 and "z" coordinates.
+      var:       Variable to be exported to GMV (TM); collocated or staggered.
+      pos:       Position of the variable.  
+    Returns:
+      none!
+    """
+
+    # Unpack tuples
+    xn, yn, zn = xyzn
+
+    # Compute cell resolutions (remember: cell resolutions)
+    nx = len(xn)-1
+    ny = len(yn)-1
+    nz = len(zn)-1
+
+    file_id = open(file_name, 'w')
+
+    # --------------------------
+    # Write the file header out
+    # --------------------------
+    file_id.write("gmvinput ascii\n")
+
+    # -------------------------------------------------------------------
+    # Write the coordinates out (remember - those are nodal coordinates)
+    # -------------------------------------------------------------------
+    file_id.write("nodev %d\n" % ((nx+1)*(ny+1)*(nz+1)))
+    for k in range(0, nz+1):
+        for j in range(0, ny+1):
+            for i in range(0, nx+1):
+                file_id.write("%12.5e %12.5e %12.5e\n" % (xn[i], yn[j], zn[k]))
+
+    # --------------------
+    # Write the cells out 
+    # --------------------
+    # file_id.write("cells %d\n" % (nx * ny * nz))
+    file_id.write("cells %d\n" % (nx*ny*nz))
+    
+    #   cells' local numbering
+    #     
+    #     ^ z
+    #     | 
+    #     | n011-------n111     
+    #     | /|         /|      
+    #     |/ |        / |      
+    #    n001-------n101|      
+    #     |  |       |  |      
+    #     |  | / y   |  |      
+    #     |  |/      |  |      
+    #     | n010-----|-n110    
+    #     | /        | /       
+    #     |/         |/        
+    #    n000-------n100 ----> 
+    #                          x 
+    for k in range(0, nz):
+        for j in range(0, ny):
+            for i in range(0, nx):
+               n000 = 1 + i + j * (nx+1) + k * ((nx+1)*(ny+1))
+               n100 = n000 + 1
+               n010 = n000 + (nx+1)
+               n110 = n000 + (nx+1) + 1
+               n001 = n000 + (nx+1)*(ny+1)
+               n101 = n001 + 1
+               n011 = n001 + (nx+1)
+               n111 = n001 + (nx+1) + 1
+
+               file_id.write("  hex 8\n")
+               file_id.write("  %d %d %d %d %d %d %d %d\n"  \
+                             % (n000,n100,n110,n010, n001,n101,n111,n011))
+
+    # ------------------------
+    # Write the variables out
+    # ------------------------
+    file_id.write("variables\n")
+
+    if pos == C:
+        val = var
+    elif v.pos == X:
+        val = avg_x(cat_x((var[ :1,:,:], var, var[-1:,:,:])))
+    elif v.pos == Y:
+        val = avg_y(cat_y((var[:, :1,:], var, var[:,-1:,:])))
+    elif v.pos == Z:
+        val = avg_z(cat_z((var[:,:, :1], var, var[:,:,-1:])))
+
+    file_id.write("unnamed 0\n")
+    c = 0
+    for k in range(0, nz):
+        for j in range(0, ny):
+            for i in range(0, nx):
+                file_id.write("%12.5e\n" % val[i,j,k])
+
+    file_id.write("endvars\n")
+
+    # --------------------------
+    # Write the file footer out
+    # --------------------------
+    file_id.write("endgmv\n")
+    
+    file_id.close()
+
+    return  # end of function
+
+    return
--- display/plot/tecplot.py
+++ display/plot/tecplot.py
@@ -10,15 +10,15 @@ from pyns.constants import *
 from pyns.operators import *
 
 # =============================================================================
-def tecplot(file_name, xyzn, variables):
+def tecplot(file_name, xyzn, vars):
 # -----------------------------------------------------------------------------
     """
     Args:
       file_name: String containing name of the file to be created.
       xyzn:      Tuple containing one-dimensional arrays with "x", "y"
                  and "z" coordinates.
-      variables: Tuple containing unknowns to be exported to Tecplot (TM).
-                 Individual variables can be either collocated or staggered.
+      vars:      Tuple containing "Unknowns" to be exported to Tecplot (TM).
+                 Individual unknowns can be either collocated or staggered.
 
     Returns:
       none!
@@ -121,6 +121,112 @@ def tecplot(file_name, xyzn, variables):
                     c = c + 1
                     if c % 4 == 0:
                         file_id.write("\n")
+
+    file_id.close()
+
+    return  # end of function
+
+# =============================================================================
+def tecplot(file_name, xyzn, var, pos):
+# -----------------------------------------------------------------------------
+    """
+    Args:
+      file_name: String containing name of the file to be created.
+      xyzn:      Tuple containing one-dimensional arrays with "x", "y"
+                 and "z" coordinates.
+      var:       Variable to be exported to Tecplot (TM)
+      pos:       Position of the variable.  
+    Returns:
+      none!
+    """
+
+    # Unpack tuples
+    xn, yn, zn = xyzn
+
+    # Compute cell resolutions (remember: cell resolutions)
+    nx = len(xn)-1
+    ny = len(yn)-1
+    nz = len(zn)-1
+
+    file_id = open(file_name, 'w')
+
+    # VisIt can't read Tecplot (TM) files which contain comments 
+    verbatim = False
+
+    # --------------------------
+    # Write the file header out
+    # --------------------------
+    if verbatim:
+        file_id.write("# File header \n")
+    file_id.write("title=\"PyNS Output\"\n")
+    file_id.write("variables=\"x\" \"y\" \"z\" \"unnamed\"")
+    file_id.write("\n")
+    file_id.write("zone i=%d"   % (nx+1) +   \
+                      " j=%d"   % (ny+1) +   \
+                      " k=%d\n" % (nz+1))
+    file_id.write("datapacking = block\n")
+    file_id.write("varlocation=([1-3]=nodal [4]=cellcentered)")
+
+    # -------------------------------------------------------------------
+    # Write the coordinates out (remember - those are nodal coordinates)
+    # -------------------------------------------------------------------
+    if verbatim:
+        file_id.write("\n# X coordinates\n")
+    c = 0                                  # column counter
+    for k in range(0, nz+1):
+        for j in range(0, ny+1):
+            for i in range(0, nx+1):
+                file_id.write("%12.5e " % xn[i])
+                c = c + 1
+                if c % 4 == 0:             # go to new line after 4th column
+                    file_id.write("\n")
+
+    if verbatim:
+        file_id.write("\n# Y coordinates\n")
+    c = 0                                  # column counter
+    for k in range(0, nz+1):
+        for j in range(0, ny+1):
+            for i in range(0, nx+1):
+                file_id.write("%12.5e " % yn[j])
+                c = c + 1
+                if c % 4 == 0:             # go to new line after 4th column
+                    file_id.write("\n")
+
+    if verbatim:
+        file_id.write("\n# Z coordinates\n")
+    c = 0                                  # column counter
+    for k in range(0, nz+1):
+        for j in range(0, ny+1):
+            for i in range(0, nx+1):
+                file_id.write("%12.5e " % zn[k])
+                c = c + 1
+                if c % 4 == 0:             # go to new line after 4th column
+                    file_id.write("\n")
+
+    # -----------------------
+    # Write the variable out
+    # -----------------------
+
+    # Average values to be written for staggered variables
+    if pos == C:
+        val = var
+    elif v.pos == X:
+        val = avg_x(cat_x((var[ :1,:,:], var, var[-1:,:,:])))
+    elif v.pos == Y:
+        val = avg_y(cat_y((var[:, :1,:], var, var[:,-1:,:])))
+    elif v.pos == Z:
+        val = avg_z(cat_z((var[:,:, :1], var, var[:,:,-1:])))
+
+    if verbatim:
+        file_id.write("\n# %s \n" % v.name)
+    c = 0
+    for k in range(0, nz):
+        for j in range(0, ny):
+            for i in range(0, nx):
+                file_id.write("%12.5e " % var[i,j,k])
+                c = c + 1
+                if c % 4 == 0:
+                    file_id.write("\n")
 
     file_id.close()
 
