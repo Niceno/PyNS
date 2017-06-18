a5704fddac455b7f3221966edababc5f79d4056d
Niceno Bojan <bojan.niceno@psi.ch>
2017-06-18 17:50:08 +0200

    New speciallized functions for plotting.

--- display/plot/__init__.py
+++ display/plot/__init__.py
@@ -5,6 +5,9 @@ Very bad, but it makes code development faster for non-IT oriendted minds.
 """
 
 # Plotting functions
-from .isolines import isolines
-from .gmv      import gmv
-from .tecplot  import tecplot
+from .isolines          import isolines
+from .gmv               import gmv
+from .gmv_nodal_uvw     import gmv_nodal_uvw
+from .tecplot           import tecplot
+from .tecplot_nodal_uvw import tecplot_nodal_uvw
+from .tecplot_one_arr   import tecplot_one_arr
