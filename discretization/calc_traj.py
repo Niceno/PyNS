4fa05e8134c506f45dff3fbe69b4a8b4d14903e4
Bojan Niceno <bojan.niceno@psi.ch>
2017-07-01 06:43:47 +0200

    A few changes to standardize the look and feel. Changed variable for browsing through particles from "i" (which is usually used for "x" coordinate" to "p" Changed cell's logical position from "x", "y" and "z" to more usual "i", "j" and "k". Removed unused imports. Uses local physical properties when entering function "langrange_interpol".

--- discretization/calc_traj.py
+++ discretization/calc_traj.py
@@ -9,24 +9,13 @@ the particles velocity.
 # Standard Python modules
 from pyns.standard import *
 
-
 #From PyNS modules
 from pyns.constants import *
-from pyns.discretization.adj_n_bnds   import adj_n_bnds
-from pyns.discretization.adj_o_bnds   import adj_o_bnds
-from pyns.discretization.obst_zero_val  import obst_zero_val
-from pyns.operators.avg import avg_x
-from pyns.operators.avg import avg_y
-from pyns.operators.avg import avg_z
-from pyns.operators.cat import cat_x
-from pyns.operators.cat import cat_y
-from pyns.operators.cat import cat_z
-from pyns.lagrangian    import * 
-import numpy as np 
+from pyns.lagrangian import * 
 
+# =============================================================================
 def calc_traj(pt, uvwn, rho, mu, xyzn, xyzc, dt, obst, n):
-    
-    
+# -----------------------------------------------------------------------------        
     """
     Args:
       pt:   The created particles velocitie components and positions.
@@ -48,72 +37,55 @@ def calc_traj(pt, uvwn, rho, mu, xyzn, xyzc, dt, obst, n):
     xn,  yn,  zn  = xyzn
     xc,  yc,  zc  = xyzc
     
-    # Extract the physical parameters of the Fluid. 
-    rho = rho[0,0,0]
-    mu = mu[0,0,0]
-    
-    obst = np.nonzero(obst)
+    obst = lnot(obst)
 
-    # Iterate through all the particles.
-    for i in range(0,n):
+    # ----------------------------------
+    # Iterate through all the particles
+    # ----------------------------------
+    for p in range(0, n):
         
-        (iu, il) = closest_node(xn, pt[i].x)
-        (ju, jl) = closest_node(yn, pt[i].y)
-        (ku, kl) = closest_node(zn, pt[i].z)
+        (iu, il) = closest_node(xn, pt[p].x)
+        (ju, jl) = closest_node(yn, pt[p].y)
+        (ku, kl) = closest_node(zn, pt[p].z)
+                
+        i = closest_cell(xc, pt[p].x)
+        j = closest_cell(yc, pt[p].y)
+        k = closest_cell(zc, pt[p].z)
         
+        # Check if particle is in an obstacle cell;
+        # if true set the particle's velocity to zero.        
+        if (i in obst[0]) and (j in obst[1]) and (k in obst[2]):
+            (pt[p].u,pt[p].v,pt[p].w) = (0, 0, 0)
         
-        x = closest_cell(xc, pt[i].x)
-        y = closest_cell(yc, pt[i].y)
-        z = closest_cell(zc, pt[i].z)
-        
-        # Check if Particle is in an Obstacle cell, if True then set the particle's 
-        # velocity to zero.
-        
-        if (x in obst[0]) and (y in obst[1]) and (z in obst[2]):
-            (pt[i].u,pt[i].v,pt[i].w) = (0, 0, 0)
-        
-        # Check to see if the particle is still in the domain, if not, the particle
-        # sticks to the surrounding boundary
-        
-        elif (min(xn) > pt[i].x or  pt[i].x > max(xn)) or \
-             (min(yn) > pt[i].y or pt[i].y > max(yn)) or \
-             (min(zn) > pt[i].z or pt[i].z > max(zn)):
-                 
-                 (pt[i].u, pt[i].v, pt[i].w) = (0, 0, 0)
-        
-        
-        # If the particle hasn't ran into trouble and hit a boundary, we calculate 
-        # its change in position and velocity. 
-        
+        # Check to see if the particle is still in the domain; 
+        # if not, the particle sticks to the surrounding boundary        
+        elif (min(xn) > pt[p].x or pt[p].x > max(xn)) or \
+             (min(yn) > pt[p].y or pt[p].y > max(yn)) or \
+             (min(zn) > pt[p].z or pt[p].z > max(zn)):                 
+                 (pt[p].u, pt[p].v, pt[p].w) = (0, 0, 0)
+                
+        # If the particle hasn't ran into trouble and hit a boundary,
+        # calculate its change in position and velocity.         
         else:
     
             # Find the Fluid velocities defined at the node.
             (u,v,w) = lagrange_interpol((un,vn,wn), (xn,yn,zn), 
-                                        (pt[i].x, pt[i].y, pt[i].z), 
+                                        (pt[p].x, pt[p].y, pt[p].z), 
                                          iu, il, ju, jl, ku, kl)
-        
-     
-            # Calculating the new particle velocities, using a fourth order Runge-kutta 
-            # scheme. The y-component has the added gravity force. 
-            
-            
-            pt[i].u = rk4(u, pt[i].u, rho, mu, dt)
-            
-            pt[i].v = rk4(v, pt[i].v, rho, mu, dt)
-            
-            pt[i].w = rk4(w, pt[i].w, rho, mu, dt)
-            
-        
-            # Updating the positions of the particles. 
-        
-            pt[i].x = pt[i].x + pt[i].u * dt 
-            
-            pt[i].y = pt[i].y + pt[i].v * dt 
-            
-            pt[i].z = pt[i].z + pt[i].w * dt
-        
-        
-    return pt[i].x, pt[i].y, pt[i].z, pt[i].u, pt[i].v, pt[i].w # end
+             
+            # Calculating the new particle velocities, 
+            # using a fourth order Runge-kutta scheme. 
+            # The y-component has the added gravity force. 
+            pt[p].u = rk4(u, pt[p].u, rho[i,j,k], mu[i,j,k], dt)            
+            pt[p].v = rk4(v, pt[p].v, rho[i,j,k], mu[i,j,k], dt)            
+            pt[p].w = rk4(w, pt[p].w, rho[i,j,k], mu[i,j,k], dt)
+                    
+            # Updating the positions of the particles.         
+            pt[p].x = pt[p].x + pt[p].u * dt            
+            pt[p].y = pt[p].y + pt[p].v * dt             
+            pt[p].z = pt[p].z + pt[p].w * dt
+                
+    return pt[p].x, pt[p].y, pt[p].z, pt[p].u, pt[p].v, pt[p].w  # end
     
 
 
