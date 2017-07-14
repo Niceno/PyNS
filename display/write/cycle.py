7fff1e6167cca95f29be1d33331c4d3058740784
Bojan Niceno <bojan.niceno@psi.ch>
2017-07-14 16:25:31 +0200

    Added "cycle.py" in "pyns.display.write".

---
+++ display/write/cycle.py
@@ -0,0 +1,20 @@
+"""
+Elegantly prints the current time step on the Python console.
+"""
+
+# =============================================================================
+def cycle(cyc):
+# -----------------------------------------------------------------------------
+    """
+    Args:
+      cyc: Cycle.
+
+    Returns:
+      None!
+    """
+
+    print("+------------+")
+    print("| Cycle = " +"%2d" % cyc + " |")
+    print("+------------+")
+
+    return  # end of function
